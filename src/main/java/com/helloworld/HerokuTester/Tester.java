ef0a7456445ab9a98c527f38e6e42cfa2d6ca9cc
Clay S <csbuiss@gmail.com>
2012-07-28 11:18:44 -0500

    just a few changes

--- src/main/java/com/helloworld/HerokuTester/Tester.java
+++ src/main/java/com/helloworld/HerokuTester/Tester.java
@@ -8,7 +8,7 @@ public class Tester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		EmbedVaadin.forApplication(MyVaadinApplication.class).openBrowser(true).withContextRootDirectory("src/main/webapp").start();
+		EmbedVaadin.forApplication(MyVaadinApplication.class).openBrowser(false).withContextRootDirectory("src/main/webapp").start();
 	}
 
 }
